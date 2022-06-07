28452269d15408f6a1c1066c8a2562c3bda9d453
Tsing <2719584131@qq.com>
2022-06-07 16:23:46 +0800

    Create get_mean_std.py

---
+++ system/get_mean_std.py
@@ -0,0 +1,18 @@
+from statistics import mean
+import numpy as np
+
+file_name = input() + '.out'
+
+acc = []
+
+with open(file_name, 'r') as f:
+    is_best = False
+    for l in f.readlines():
+        if is_best:
+            acc.append(float(l))
+            is_best = False
+        elif 'Best' in l:
+            is_best = True
+
+print(acc)
+print(mean(acc)*100, np.std(acc)*100)
