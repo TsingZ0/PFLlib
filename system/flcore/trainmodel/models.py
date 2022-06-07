97b355e4499d1bb82b7617d0959228c6e4775996
Tsing <2719584131@qq.com>
2022-06-07 16:22:20 +0800

    Update models.py

--- system/flcore/trainmodel/models.py
+++ system/flcore/trainmodel/models.py
@@ -19,8 +19,9 @@ class LocalModel(nn.Module):
         return out
         
 
+# # https://github.com/katsura-jp/fedavg.pytorch/blob/master/src/models/cnn.py
 # class FedAvgCNN(nn.Module):
-#     def __init__(self, in_features=1, num_classes=10, dim=1024, dropout=0.0):
+#     def __init__(self, in_features=1, num_classes=10, dim=1024):
 #         super().__init__()
 #         self.conv1 = nn.Conv2d(in_features,
 #                                32,
@@ -40,53 +41,52 @@ class LocalModel(nn.Module):
 #         self.act = nn.ReLU(inplace=True)
 #         self.maxpool = nn.MaxPool2d(kernel_size=(2, 2))
 
-#         self.dropout = nn.Dropout(p=dropout)
-
 #     def forward(self, x):
 #         x = self.act(self.conv1(x))
 #         x = self.maxpool(x)
-#         x = self.dropout(x)
 #         x = self.act(self.conv2(x))
 #         x = self.maxpool(x)
-#         x = self.dropout(x)
 #         x = torch.flatten(x, 1)
 #         x = self.act(self.fc1(x))
-#         x = self.dropout(x)
 #         x = self.fc(x)
 #         return x
 
-
-# https://github.com/katsura-jp/fedavg.pytorch/blob/master/src/models/cnn.py
 class FedAvgCNN(nn.Module):
     def __init__(self, in_features=1, num_classes=10, dim=1024):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_features,
-                               32,
-                               kernel_size=5,
-                               padding=0,
-                               stride=1,
-                               bias=True)
-        self.conv2 = nn.Conv2d(32,
-                               64,
-                               kernel_size=5,
-                               padding=0,
-                               stride=1,
-                               bias=True)
-        self.fc1 = nn.Linear(dim, 512)
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_features,
+                        32,
+                        kernel_size=5,
+                        padding=0,
+                        stride=1,
+                        bias=True),
+            nn.ReLU(inplace=True), 
+            nn.MaxPool2d(kernel_size=(2, 2))
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32,
+                        64,
+                        kernel_size=5,
+                        padding=0,
+                        stride=1,
+                        bias=True),
+            nn.ReLU(inplace=True), 
+            nn.MaxPool2d(kernel_size=(2, 2))
+        )
+        self.fc1 = nn.Sequential(
+            nn.Linear(dim, 512), 
+            nn.ReLU(inplace=True)
+        )
         self.fc = nn.Linear(512, num_classes)
 
-        self.act = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=(2, 2))
-
     def forward(self, x):
-        x = self.act(self.conv1(x))
-        x = self.maxpool(x)
-        x = self.act(self.conv2(x))
-        x = self.maxpool(x)
-        x = torch.flatten(x, 1)
-        x = self.act(self.fc1(x))
-        x = self.fc(x)
-        return x
+        out = self.conv1(x)
+        out = self.conv2(out)
+        out = torch.flatten(out, 1)
+        out = self.fc1(out)
+        out = self.fc(out)
+        return out
 
 # ====================================================================================================================
 
