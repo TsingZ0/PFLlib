46e375d7d2520dae39f64766510822bcc99e9957
Tsing <2719584131@qq.com>
2022-09-02 11:12:10 +0800

    Update clientapfl.py

--- system/flcore/clients/clientapfl.py
+++ system/flcore/clients/clientapfl.py
@@ -14,7 +14,8 @@ class clientAPFL(Client):
         self.optimizer = torch.optim.SGD(self.model.parameters(), lr=self.learning_rate)
 
         self.alpha = args.alpha
-        self.w_local = copy.deepcopy(list(self.model.parameters()))
+        self.model_per = copy.deepcopy(self.model)
+        self.optimizer_per = torch.optim.SGD(self.model_per.parameters(), lr=self.learning_rate)
 
     def train(self):
         trainloader = self.load_train_data()
@@ -23,10 +24,6 @@ class clientAPFL(Client):
 
         # self.model.to(self.device)
         self.model.train()
-
-        w_loc_new = []
-        for p, lp in zip(self.model.parameters(), self.w_local):
-            w_loc_new.append(self.alpha * (p + lp))
 
         max_local_steps = self.local_steps
         if self.train_slow:
@@ -47,32 +44,28 @@ class clientAPFL(Client):
                 loss.backward()
                 self.optimizer.step()
 
-        wt = copy.deepcopy(list(self.model.parameters()))
-        self.update_parameters(self.model, w_loc_new)
-        
-        for step in range(max_local_steps):
-            for i, (x, y) in enumerate(trainloader):
-                if type(x) == type([]):
-                    x[0] = x[0].to(self.device)
-                else:
-                    x = x.to(self.device)
-                y = y.to(self.device)
-                if self.train_slow:
-                    time.sleep(0.1 * np.abs(np.random.rand()))
-                self.optimizer.zero_grad()
-                output = self.model(x)
-                loss = self.loss(output, y) * self.alpha
-                loss.backward()
-                self.optimizer.step()
+                self.optimizer_per.zero_grad()
+                output_per = self.model_per(x)
+                loss_per = self.loss(output_per, y)
+                loss_per.backward()
+                self.optimizer_per.step()
 
-        # self.model.cpu()
+                self.alpha_update()
 
-        w_local_bar = copy.deepcopy(list(self.model.parameters()))
-        for lp, lp_bar, lp_new, pt in zip(self.w_local, w_local_bar, w_loc_new, wt):
-            lp.data = lp_bar - lp_new + lp
-            pt.data = (1 - self.alpha) * pt + self.alpha * lp
-
-        self.update_parameters(self.model, wt)
+        for lp, p in zip(self.model_per.parameters(), self.model.parameters()):
+            lp.data = (1 - self.alpha) * p + self.alpha * lp
 
         self.train_time_cost['num_rounds'] += 1
         self.train_time_cost['total_cost'] += time.time() - start_time
+
+    # https://github.com/MLOPTPSU/FedTorch/blob/b58da7408d783fd426872b63fbe0c0352c7fa8e4/fedtorch/comms/utils/flow_utils.py#L240
+    def alpha_update(self):
+        grad_alpha = 0
+        for l_params, p_params in zip(self.model.parameters(), self.model_per.parameters()):
+            dif = p_params.data - l_params.data
+            grad = self.alpha * p_params.grad.data + (1-self.alpha) * l_params.grad.data
+            grad_alpha += dif.view(-1).T.dot(grad.view(-1))
+        
+        grad_alpha += 0.02 * self.alpha
+        self.alpha = self.alpha - self.learning_rate * grad_alpha
+        self.alpha = np.clip(self.alpha.item(), 0.0, 1.0)
