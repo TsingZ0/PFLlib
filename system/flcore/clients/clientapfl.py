fad4cd9fec3371375b2d86846bdeb0fba48f1dc2
Tsing <2719584131@qq.com>
2023-01-05 16:21:16 +0800

    update APFL

--- system/flcore/clients/clientapfl.py
+++ system/flcore/clients/clientapfl.py
@@ -5,6 +5,8 @@ import numpy as np
 import time
 from flcore.clients.clientbase import Client
 
+from sklearn.preprocessing import label_binarize
+from sklearn import metrics
 
 class clientAPFL(Client):
     def __init__(self, args, id, train_samples, test_samples, **kwargs):
@@ -69,3 +71,34 @@ class clientAPFL(Client):
         grad_alpha += 0.02 * self.alpha
         self.alpha = self.alpha - self.learning_rate * grad_alpha
         self.alpha = np.clip(self.alpha.item(), 0.0, 1.0)
+
+    def test_metrics(self):
+        testloaderfull = self.load_test_data()
+        self.model_per.eval()
+
+        test_acc = 0
+        test_num = 0
+        y_prob = []
+        y_true = []
+        
+        with torch.no_grad():
+            for x, y in testloaderfull:
+                if type(x) == type([]):
+                    x[0] = x[0].to(self.device)
+                else:
+                    x = x.to(self.device)
+                y = y.to(self.device)
+                output = self.model_per(x)
+
+                test_acc += (torch.sum(torch.argmax(output, dim=1) == y)).item()
+                test_num += y.shape[0]
+
+                y_prob.append(output.detach().cpu().numpy())
+                y_true.append(label_binarize(y.detach().cpu().numpy(), classes=np.arange(self.num_classes)))
+
+        y_prob = np.concatenate(y_prob, axis=0)
+        y_true = np.concatenate(y_true, axis=0)
+
+        auc = metrics.roc_auc_score(y_true, y_prob, average='micro')
+        
+        return test_acc, test_num, auc
