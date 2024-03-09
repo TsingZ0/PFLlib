3edd46280ffca740816f17ccd78f096bd5cc0f3a
Tsing <2719584131@qq.com>
2024-03-09 14:10:57 +0800

    add six datasets

---
+++ dataset/generate_StanfordCars.py
@@ -0,0 +1,71 @@
+import numpy as np
+import os
+import sys
+import random
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from utils.dataset_utils import check, separate_data, split_data, save_file
+
+
+random.seed(1)
+np.random.seed(1)
+num_clients = 20
+dir_path = "StanfordCars/"
+
+
+# Allocate data to users
+def generate_StanfordCars(dir_path, num_clients, niid, balance, partition):
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+        
+    # Setup directory for train/test data
+    config_path = dir_path + "config.json"
+    train_path = dir_path + "train/"
+    test_path = dir_path + "test/"
+
+    if check(config_path, train_path, test_path, num_clients, niid, balance, partition):
+        return
+
+    dataset_image = []
+    dataset_label = []
+        
+    # Get StanfordCars data
+    transform = transforms.Compose(
+        [transforms.Resize((64, 64)), 
+        transforms.ToTensor(), 
+        transforms.Normalize((0.5), (0.5))]
+    )
+
+    def load_data(split="train"):
+        trainset = torchvision.datasets.StanfordCars(
+            root=dir_path+"rawdata", split=split, download=True, transform=transform)
+        trainloader = torch.utils.data.DataLoader(
+            trainset, batch_size=len(trainset), shuffle=False)
+        for _, train_data in enumerate(trainloader, 0):
+            trainset.data, trainset.targets = train_data
+        dataset_image.extend(trainset.data.cpu().detach().numpy())
+        dataset_label.extend(trainset.targets.cpu().detach().numpy())
+
+    load_data("train")
+    load_data("test")
+
+    dataset_image = np.array(dataset_image)
+    dataset_label = np.array(dataset_label)
+
+    num_classes = len(set(dataset_label))
+    print(f'Number of classes: {num_classes}')
+
+    X, y, statistic = separate_data((dataset_image, dataset_label), num_clients, num_classes, 
+                                    niid, balance, partition, class_per_client=20)
+    train_data, test_data = split_data(X, y)
+    save_file(config_path, train_path, test_path, train_data, test_data, num_clients, num_classes, 
+        statistic, niid, balance, partition)
+
+
+if __name__ == "__main__":
+    niid = True if sys.argv[1] == "noniid" else False
+    balance = True if sys.argv[2] == "balance" else False
+    partition = sys.argv[3] if sys.argv[3] != "-" else None
+
+    generate_StanfordCars(dir_path, num_clients, niid, balance, partition)
---
+++ dataset/utils/language_utils.py
@@ -0,0 +1,149 @@
+"""Utils for language models."""
+
+import re
+import numpy as np
+import json
+
+
+# ------------------------
+# utils for shakespeare dataset
+
+ALL_LETTERS = "\n !\"&'(),-.0123456789:;>?ABCDEFGHIJKLMNOPQRSTUVWXYZ[]abcdefghijklmnopqrstuvwxyz}"
+NUM_LETTERS = len(ALL_LETTERS)
+print('num of letters:', NUM_LETTERS)
+
+
+def letter_to_index(letter):
+    '''returns one-hot representation of given letter
+    '''
+    index = ALL_LETTERS.find(letter)
+    return index
+
+def _one_hot(index, size):
+    '''returns one-hot vector with given size and value 1 at given index
+    '''
+    vec = [0 for _ in range(size)]
+    vec[int(index)] = 1
+    return vec
+
+
+def letter_to_vec(letter):
+    '''returns one-hot representation of given letter
+    '''
+    index = ALL_LETTERS.find(letter)
+    return _one_hot(index, NUM_LETTERS)
+
+
+def word_to_indices(word):
+    '''returns a list of character indices
+
+    Args:
+        word: string
+    
+    Return:
+        indices: int list with length len(word)
+    '''
+    indices = []
+    for c in word:
+        indices.append(ALL_LETTERS.find(c))
+    return indices
+
+
+# ------------------------
+# utils for sent140 dataset
+
+
+def split_line(line):
+    '''split given line/phrase into list of words
+
+    Args:
+        line: string representing phrase to be split
+    
+    Return:
+        list of strings, with each string representing a word
+    '''
+    return re.findall(r"[\w']+|[.,!?;]", line)
+
+
+def _word_to_index(word, indd):
+    '''returns index of given word based on given lookup dictionary
+
+    returns the length of the lookup dictionary if word not found
+
+    Args:
+        word: string
+        indd: dictionary with string words as keys and int indices as values
+    '''
+    if word in indd:
+        return indd[word]
+    else:
+        return len(indd)
+
+
+def line_to_indices(line, word2id, max_words=25):
+    '''converts given phrase into list of word indices
+    
+    if the phrase has more than max_words words, returns a list containing
+    indices of the first max_words words
+    if the phrase has less than max_words words, repeatedly appends integer 
+    representing unknown index to returned list until the list's length is 
+    max_words
+
+    Args:
+        line: string representing phrase/sequence of words
+        word2id: dictionary with string words as keys and int indices as values
+        max_words: maximum number of word indices in returned list
+
+    Return:
+        indl: list of word indices, one index for each word in phrase
+    '''
+    unk_id = len(word2id)
+    line_list = split_line(line) # split phrase in words
+    indl = [word2id[w] if w in word2id else unk_id for w in line_list[:max_words]]
+    indl += [unk_id]*(max_words-len(indl))
+    return indl
+
+
+def bag_of_words(line, vocab):
+    '''returns bag of words representation of given phrase using given vocab
+
+    Args:
+        line: string representing phrase to be parsed
+        vocab: dictionary with words as keys and indices as values
+
+    Return:
+        integer list
+    '''
+    bag = [0]*len(vocab)
+    words = split_line(line)
+    for w in words:
+        if w in vocab:
+            bag[vocab[w]] += 1
+    return bag
+
+
+def get_word_emb_arr(path):
+    with open(path, 'r') as inf:
+        embs = json.load(inf)
+    vocab = embs['vocab']
+    word_emb_arr = np.array(embs['emba'])
+    indd = {}
+    for i in range(len(vocab)):
+        indd[vocab[i]] = i
+    vocab = {w: i for i, w in enumerate(embs['vocab'])}
+    return word_emb_arr, indd, vocab
+
+
+def val_to_vec(size, val):
+    """Converts target into one-hot.
+
+    Args:
+        size: Size of vector.
+        val: Integer in range [0, size].
+    Returns:
+         vec: one-hot vector with a 1 in the val element.
+    """
+    assert 0 <= val < size
+    vec = [0 for _ in range(size)]
+    vec[int(val)] = 1
+    return vec
