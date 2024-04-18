50cad10ab2404c6d4ad460796512cc9c1568b945
NigeloYang <yangqiantao@outlook.com>
2024-04-18 19:44:20 +0800

    add data visualization

--- dataset/show_data_distribution.py
+++ dataset/show_data_distribution.py
@@ -2,6 +2,7 @@
 # @Time    : 2024/4/11
 # By Qiantao Yang
 
+import argparse
 import numpy as np
 import os
 import sys
@@ -19,45 +20,68 @@ def get_data(dataset_name):
         dir_path = "Cifar10/"
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
+        with open(dir_path + 'config.json') as f:
+            client_dict = json.load(f)
+        
         transform = transforms.Compose(
             [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-        testset = torchvision.datasets.MNIST(root=dir_path + "rawdata", train=False, download=True, transform=transform)
-        with open(dir_path + 'config.json') as f:
-            client_data = json.load(f)
+        dataset = torchvision.datasets.CIFAR10(root=dir_path + "rawdata", train=False, download=True,
+                                               transform=transform)
+        dataset_label = np.array(dataset.classes)
     elif dataset_name == 'Cifar100':
         dir_path = "Cifar100/"
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
+        with open(dir_path + 'config.json') as f:
+            client_dict = json.load(f)
+        
         transform = transforms.Compose(
             [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-        testset = torchvision.datasets.MNIST(root=dir_path + "rawdata", train=False, download=True, transform=transform)
-        with open(dir_path + 'config.json') as f:
-            client_data = json.load(f)
+        dataset = torchvision.datasets.CIFAR100(root=dir_path + "rawdata", train=False, download=True,
+                                                transform=transform)
+        dataset_label = np.array(dataset.classes)
     elif dataset_name == 'FashionMNIST':
         dir_path = "FashionMNIST/"
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
-        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize([0.5], [0.5])])
-        testset = torchvision.datasets.MNIST(root=dir_path + "rawdata", train=False, download=True, transform=transform)
         with open(dir_path + 'config.json') as f:
-            client_data = json.load(f)
+            client_dict = json.load(f)
+        
+        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize([0.5], [0.5])])
+        dataset = torchvision.datasets.FashionMNIST(root=dir_path + "rawdata", train=False, download=True,
+                                                    transform=transform)
+        dataset_label = np.array(dataset.classes)
+        print(dataset_label)
+    
+    elif dataset_name == 'Flowers102':
+        dir_path = "Flowers102/"
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        with open(dir_path + 'config.json') as f:
+            client_dict = json.load(f)
+        with open('dataset_label/cat_to_name.json') as f:
+            labels = json.load(f)
+        dataset_label = []
+        for i in range(1, len(labels.keys()) + 1):
+            dataset_label.append(labels[str(i)])
     elif dataset_name == 'MNIST':
         dir_path = "MNIST/"
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
-        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize([0.5], [0.5])])
-        testset = torchvision.datasets.MNIST(root=dir_path + "rawdata", train=False, download=True, transform=transform)
         with open(dir_path + 'config.json') as f:
             client_dict = json.load(f)
+        
+        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize([0.5], [0.5])])
+        dataset = torchvision.datasets.MNIST(root=dir_path + "rawdata", train=False, download=True, transform=transform)
+        dataset_label = np.array(dataset.classes)
     else:
-        print('The dataset is not available at this time')
+        print('There are only a few data sets, e.g. [MNIST, FashionMNIST, Cifar10, Cifar100, Flowers102]')
     
-    dataset_label = np.array(testset.classes)
     return dataset_label, client_dict
 
 
-def show_data_distribution(dataset_name):
-    dataset_label, client_dict = get_data(dataset_name)
+def show_data_distribution(args):
+    dataset_label, client_dict = get_data(args.datasetname)
     
     label_distribution = [[] for _ in range(len(dataset_label))]
     
@@ -86,10 +110,17 @@ def show_data_distribution(dataset_name):
     plt.ylabel("Number of samples")
     plt.xlabel("Client ID")
     plt.legend()
-    plt.title("Dataset {} Distribution: {} Non-IID: {}".format(dataset_name, client_dict['partition'],
+    plt.title("Dataset {} Distribution: {} Non-IID: {}".format(args.datasetname, client_dict['partition'],
                                                                client_dict['partition']))
     plt.show()
 
 
 if __name__ == "__main__":
-    show_data_distribution('MNIST')
+    parser = argparse.ArgumentParser()
+    # general
+    parser.add_argument("-dsname", "--datasetname", type=str, default="MNIST",
+                        help="input dataset name, e.g. [MNIST,FashionMNIST,Cifar10,Cifar100,Flowers102]")
+    
+    args = parser.parse_args()
+    
+    show_data_distribution(args)
