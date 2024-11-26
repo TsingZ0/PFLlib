a9ab8c9cd78c760784812c7cdc30cf53558f1ade
Tsing <2719584131@qq.com>
2024-11-26 10:31:29 +0800

    Update main.py

--- system/main.py
+++ system/main.py
@@ -399,7 +399,7 @@ if __name__ == "__main__":
                         choices=["cpu", "cuda"])
     parser.add_argument('-did', "--device_id", type=str, default="0")
     parser.add_argument('-data', "--dataset", type=str, default="MNIST")
-    parser.add_argument('-nb', "--num_classes", type=int, default=10)
+    parser.add_argument('-ncl', "--num_classes", type=int, default=10)
     parser.add_argument('-m', "--model", type=str, default="CNN")
     parser.add_argument('-lbs', "--batch_size", type=int, default=10)
     parser.add_argument('-lr', "--local_learning_rate", type=float, default=0.005,
