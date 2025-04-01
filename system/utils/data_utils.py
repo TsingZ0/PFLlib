e138dde570d80bb2bc1ed33788f984ea85bfd525
Tsing <2719584131@qq.com>
2025-04-01 20:09:31 +0800

    Update data_utils.py

--- system/utils/data_utils.py
+++ system/utils/data_utils.py
@@ -5,82 +5,42 @@ import torch
 
 def read_data(dataset, idx, is_train=True):
     if is_train:
-        train_data_dir = os.path.join('../dataset', dataset, 'train/')
-
-        train_file = train_data_dir + str(idx) + '.npz'
-        with open(train_file, 'rb') as f:
-            train_data = np.load(f, allow_pickle=True)['data'].tolist()
-
-        return train_data
-
+        data_dir = os.path.join('../dataset', dataset, 'train/')
     else:
-        test_data_dir = os.path.join('../dataset', dataset, 'test/')
+        data_dir = os.path.join('../dataset', dataset, 'test/')
 
-        test_file = test_data_dir + str(idx) + '.npz'
-        with open(test_file, 'rb') as f:
-            test_data = np.load(f, allow_pickle=True)['data'].tolist()
+    file = data_dir + str(idx) + '.npz'
+    with open(file, 'rb') as f:
+        data = np.load(f, allow_pickle=True)['data'].tolist()
+    return data
 
-        return test_data
 
-
-def read_client_data(dataset, idx, is_train=True):
+def read_client_data(dataset, idx, is_train=True, few_shot=0):
+    data = read_data(dataset, idx, is_train)
     if "News" in dataset:
-        return read_client_data_text(dataset, idx, is_train)
+        data_list = process_text(data)
     elif "Shakespeare" in dataset:
-        return read_client_data_Shakespeare(dataset, idx)
-
-    if is_train:
-        train_data = read_data(dataset, idx, is_train)
-        X_train = torch.Tensor(train_data['x']).type(torch.float32)
-        y_train = torch.Tensor(train_data['y']).type(torch.int64)
-
-        train_data = [(x, y) for x, y in zip(X_train, y_train)]
-        return train_data
+        data_list = process_Shakespeare(data)
     else:
-        test_data = read_data(dataset, idx, is_train)
-        X_test = torch.Tensor(test_data['x']).type(torch.float32)
-        y_test = torch.Tensor(test_data['y']).type(torch.int64)
-        test_data = [(x, y) for x, y in zip(X_test, y_test)]
-        return test_data
+        data_list = process_image(data)
+    return data_list
+
+def process_image(data):
+    X = torch.Tensor(data['x']).type(torch.float32)
+    y = torch.Tensor(data['y']).type(torch.int64)
+    return [(x, y) for x, y in zip(X, y)]
 
 
-def read_client_data_text(dataset, idx, is_train=True):
-    if is_train:
-        train_data = read_data(dataset, idx, is_train)
-        X_train, X_train_lens = list(zip(*train_data['x']))
-        y_train = train_data['y']
-
-        X_train = torch.Tensor(X_train).type(torch.int64)
-        X_train_lens = torch.Tensor(X_train_lens).type(torch.int64)
-        y_train = torch.Tensor(train_data['y']).type(torch.int64)
-
-        train_data = [((x, lens), y) for x, lens, y in zip(X_train, X_train_lens, y_train)]
-        return train_data
-    else:
-        test_data = read_data(dataset, idx, is_train)
-        X_test, X_test_lens = list(zip(*test_data['x']))
-        y_test = test_data['y']
-
-        X_test = torch.Tensor(X_test).type(torch.int64)
-        X_test_lens = torch.Tensor(X_test_lens).type(torch.int64)
-        y_test = torch.Tensor(test_data['y']).type(torch.int64)
-
-        test_data = [((x, lens), y) for x, lens, y in zip(X_test, X_test_lens, y_test)]
-        return test_data
+def process_text(data):
+    X, X_lens = list(zip(*data['x']))
+    y = data['y']
+    X = torch.Tensor(X).type(torch.int64)
+    X_lens = torch.Tensor(X_lens).type(torch.int64)
+    y = torch.Tensor(data['y']).type(torch.int64)
+    return [((x, lens), y) for x, lens, y in zip(X, X_lens, y)]
 
 
-def read_client_data_Shakespeare(dataset, idx, is_train=True):
-    if is_train:
-        train_data = read_data(dataset, idx, is_train)
-        X_train = torch.Tensor(train_data['x']).type(torch.int64)
-        y_train = torch.Tensor(train_data['y']).type(torch.int64)
-
-        train_data = [(x, y) for x, y in zip(X_train, y_train)]
-        return train_data
-    else:
-        test_data = read_data(dataset, idx, is_train)
-        X_test = torch.Tensor(test_data['x']).type(torch.int64)
-        y_test = torch.Tensor(test_data['y']).type(torch.int64)
-        test_data = [(x, y) for x, y in zip(X_test, y_test)]
-        return test_data
-
+def process_Shakespeare(data):
+    X = torch.Tensor(data['x']).type(torch.int64)
+    y = torch.Tensor(data['y']).type(torch.int64)
+    return [(x, y) for x, y in zip(X, y)]
